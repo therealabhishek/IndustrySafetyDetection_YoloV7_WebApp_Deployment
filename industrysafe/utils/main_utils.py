06ecd034032477e78adab020ce4ed349e6bbb9c9
Abhishek Kulkarni <kul.abhishake@gmail.com>
2023-08-07 16:50:05 +0530

    requirements, logger, exception, config added

---
+++ industrysafe/utils/main_utils.py
@@ -0,0 +1,28 @@
+import sys
+import yaml
+import base64
+
+from industrysafe.exception import IndustryException
+from industrysafe.logger import logging
+
+
+def read_yaml_file(file_path: str) -> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            logging.info("Read yaml file successfully")
+            return yaml.safe_load(yaml_file)
+
+    except Exception as e:
+        raise IndustryException(e, sys) from e
+
+
+def decodeImage(imgstring, fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open("./data/" + fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+
+
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
