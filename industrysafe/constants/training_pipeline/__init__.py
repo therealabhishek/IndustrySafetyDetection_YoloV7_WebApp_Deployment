e899489a96bcd284dddf169b8c1c8fac6451c579
Abhishek Kulkarni <kul.abhishake@gmail.com>
2023-08-08 19:21:38 +0530

    data validation stage completed

--- industrysafe/constants/training_pipeline/__init__.py
+++ industrysafe/constants/training_pipeline/__init__.py
@@ -13,3 +13,14 @@ DATA_INGESTION_FEATURE_STORE_DIR = "feature_store"
 DATA_INGESTION_S3_DATA_NAME = "isd_data_mini.zip"
 
 DATA_BUCKET_NAME = "indsafe-data"
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["images", "labels", "classes.names", "train.txt", "val.txt"]
--- industrysafe/pipeline/trainpipeline.py
+++ industrysafe/pipeline/trainpipeline.py
@@ -2,19 +2,19 @@ import sys, os
 from industrysafe.logger import logging
 from industrysafe.exception import IndustryException
 from industrysafe.components.data_ingestion import DataIngestion
+from industrysafe.components.data_validation import DataValidation
 from industrysafe.configuration.s3_operations import S3Operation
 
+from industrysafe.entity.config_entity import DataIngestionConfig, DataValidationConfig
 
-from industrysafe.entity.config_entity import DataIngestionConfig
-
-
-from industrysafe.entity.artifact_entity import DataIngestionArtifact
+from industrysafe.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
         self.s3_operations = S3Operation()
 
 
@@ -41,11 +41,36 @@ class TrainPipeline:
             raise IndustryException(e, sys)
         
 
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
 
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+            logging.info("Exited the start_data_validation method of TrainPipeline class")
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise IndustryException(e, sys) from e
+
+        
 
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
 
         
         except Exception as e:
